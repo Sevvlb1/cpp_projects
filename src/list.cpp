196919e6348dcae99f87d5839bde783f274fbe0f
sevvlb1 <contact@sevvlb1.com>
2025-05-26 19:30:00 +0300

    Updating the delete function

--- src/list.cpp
+++ src/list.cpp
@@ -51,21 +51,25 @@ void List::add_item()
 void List::delete_item()
 {
     cout << "*** Delete Item ***\n";
-    cout << "Select an item index number to delete\n";
+    cout << "Select an item index number to delete:\n ";
 
     if (list.size())
     {
 
         for (unsigned int i = 0; i < list.size(); i++)
         {
-            cout << i << ": " << list[i] << endl;
+            cout << i << ": " << list[i] << "\n";
         }
+        int choiceNum;
+        cin >> choiceNum;
+        list.erase(list.begin() + choiceNum);
     }
     else
     {
 
         cout << "No items in the list to delete.\n";
     }
+    print_menu();
 }
 
 void List::print_list(){
